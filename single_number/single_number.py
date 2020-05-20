10036d64085e86696e2f089a685efd6a819b7e2a
Allie Robinson <allierob93@gmail.com>
2020-05-20 17:36:35 -0500

    completed single_number and moving_zeroes

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,25 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # if the number has a duplicate, ignore
+    # if the number does not have a duplicate, return it 
+    # each each item in array against each other item in the 
+    # array except itself
+    num = arr[0]
+    for i in range(1,len(arr)):
+        num = num ^ arr[i]
+    return num
+    
+    # uses exclusive or operator 
+    # returns like:
+    # 9 ^ (0 ^ 0) ^ (1 ^ 1) ^ (3 ^ 3) ^ (4 ^ 4) ^ (5 ^ 5)
+    # = 9 ^ 0 ^ 0 ^ 0 ^ 0 ^ 0
+    # = 9 ^ 0 
+    # = 9
+    # Because: XOR of a num with itself is 0. 
+    # But XOR of a num by itself is the num. 
 
-    pass
+    
 
 
 if __name__ == '__main__':
